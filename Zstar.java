84c65fe2ac96356f55d5711003ccb4423c909e90
Mounik <gonguramounik@gmail.com>
2024-04-11 19:11:25 +0530

    project

---
+++ Zstar.java
@@ -0,0 +1,26 @@
+package printstars;
+
+public class Zstar {
+
+	public static void main(String[] args) {
+		
+		int i,j,n = 5;
+		
+		for(i = 1;i <= n; i++)
+		{
+			for(j = 1; j <= n; j++)
+			{
+				if(i == 1 || i == n || j == n - i)
+				{
+					System.out.print("*" + " ");
+				}else
+				{
+					System.out.print("  " + " ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
